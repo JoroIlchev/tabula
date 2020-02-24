d254c8461be2d6c60900aa87ffaadb82f49915e3
Lachezar Balev <lachezar.balev@gmail.com>
2020-02-24 17:56:10 +0200

    Getting forward with the announcements.

--- src/main/java/bg/softuni/tabula/announcement/AnnouncementController.java
+++ src/main/java/bg/softuni/tabula/announcement/AnnouncementController.java
@@ -1,5 +1,6 @@
 package bg.softuni.tabula.announcement;
 
+import bg.softuni.tabula.announcement.dto.AnnouncementDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,4 +13,12 @@ public class AnnouncementController {
     model.addAttribute("active", "announcements");
     return "announcement/announcements";
   }
+
+  @GetMapping("/announcements/new")
+  public String newAnnouncement(Model model) {
+    model.addAttribute("active", "announcements");
+    model.addAttribute("formData", new AnnouncementDTO());
+    return "announcement/new";
+  }
+
 }
